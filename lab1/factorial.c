1174b0f58eba577198a9fea24e80dd065403986b
Ayush Katiyar <83917413+AyushKatiyar-xyz@users.noreply.github.com>
2021-05-17 19:14:40 +0530

    Add files via upload

---
+++ lab1/factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    // factorial
+    int a=4,fac=1,i=1;
+    scanf("%d",&a);
+  
+    while (i<=a)
+    {
+        fac *=i;
+        i++;
+    }
+    printf("the value is %d",fac);
+
+
+    return 0;
+
+}
